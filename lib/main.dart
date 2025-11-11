d1403046988736188f31d8fb90e6a36fe55bd84b
Siljo Denny <siljodennyjohn>
2025-11-11 10:42:02 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SpaceZUI',
+      
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        
+        scaffoldBackgroundColor: Colors.white
       ),
       home: Homescreen(),
     );
--- lib/presentation/widgets/couponcard.dart
+++ lib/presentation/widgets/couponcard.dart
@@ -24,7 +24,7 @@ class _CouponcardState extends State<Couponcard> {
     return Padding(
       padding: const EdgeInsets.only(left: 20, right: 20),
       child: SizedBox(
-        width: double.infinity,
+        width: MediaQuery.of(context).size.width,
 
         child: Row(
           children: [
@@ -54,110 +54,113 @@ class _CouponcardState extends State<Couponcard> {
               ),
             ),
 
-            CustomPaint(
-              painter: LeftDashedBorderPainter(),
-              child: Container(
-                width: 277,
-                height: 184,
-
-                decoration: BoxDecoration(color: Color(0XFFFDF9F7)),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 12.0,
-                    vertical: 20.0,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 241,
-                        height: 30,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              widget.stayType,
-                              style: TextStyle(
-                                fontFamily: "semibold",
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Row(
-                              children: [
-                                appliedButton
-                                    ? Icon(Icons.check_circle,size: 18,)
-                                    : Image.asset(
-                                        "assets/images/shoppingmode.png",
-                                        width: 18,
-                                      ),
-                                SizedBox(width: 3),
-                                InkWell(
-                                  onTap: () {
-                                    setState(() {
-                                      appliedButton = !appliedButton;
-                                    });
-                                  },
-                                  child: appliedButton
-                                      ? Text(
-                                          "Applied",
-                                          style: TextStyle(
-                                            color: Colors.green,
-                                            fontFamily: "semibold",
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        )
-                                      : Text(
-                                          "Apply",
-                                          style: TextStyle(
-                                            color: Color(0XFF874B2C),
-                                            fontFamily: "semibold",
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
+            Expanded(
+              child: CustomPaint(
+                painter: LeftDashedBorderPainter(),
+                child: Container(
+                  
+                  
+                  height: 184,
+              
+                  decoration: BoxDecoration(color: Color(0XFFFDF9F7)),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12.0,
+                      vertical: 20.0,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 241,
+                          height: 30,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                widget.stayType,
+                                style: TextStyle(
+                                  fontFamily: "semibold",
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                              ],
+                              ),
+                              Row(
+                                children: [
+                                  appliedButton
+                                      ? Icon(Icons.check_circle,size: 18,)
+                                      : Image.asset(
+                                          "assets/images/shoppingmode.png",
+                                          width: 18,
+                                        ),
+                                  SizedBox(width: 3),
+                                  InkWell(
+                                    onTap: () {
+                                      setState(() {
+                                        appliedButton = !appliedButton;
+                                      });
+                                    },
+                                    child: appliedButton
+                                        ? Text(
+                                            "Applied",
+                                            style: TextStyle(
+                                              color: Colors.green,
+                                              fontFamily: "semibold",
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          )
+                                        : Text(
+                                            "Apply",
+                                            style: TextStyle(
+                                              color: Color(0XFF874B2C),
+                                              fontFamily: "semibold",
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          width: 241,
+                          height: 60,
+                          child: Text(
+                            widget.offer,
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontFamily: "semibold",
+                              fontWeight: FontWeight.w100,
+                              color: Color(0XFF7D817D),
                             ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: 241,
-                        height: 60,
-                        child: Text(
-                          widget.offer,
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontFamily: "semibold",
-                            fontWeight: FontWeight.w100,
-                            color: Color(0XFF7D817D),
-                          ),
-                          textAlign: TextAlign.start,
-                        ),
-                      ),
-                      Divider(
-                        thickness: 1,
-                        color: Color(0XFFE5E6E5),
-                        endIndent: 10,
-                      ),
-                      SizedBox(
-                        width: 80,
-                        height: 28,
-                        child: Text(
-                          "Read more",
-                          style: TextStyle(
-                            color: Color(0XFF7D817D),
-                            fontFamily: "semibold",
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
+                            textAlign: TextAlign.start,
                           ),
                         ),
-                      ),
-                    ],
+                        Divider(
+                          thickness: 1,
+                          color: Color(0XFFE5E6E5),
+                          endIndent: 10,
+                        ),
+                        SizedBox(
+                          width: 80,
+                          height: 28,
+                          child: Text(
+                            "Read more",
+                            style: TextStyle(
+                              color: Color(0XFF7D817D),
+                              fontFamily: "semibold",
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
